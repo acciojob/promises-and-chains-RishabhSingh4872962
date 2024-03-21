d4c55b8e370e2b9665943a9a1b121280676fbfcc
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-03-21 09:23:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+ function handleSubmit() {
+	this.event.preventDefault();
+	console.log(this.event.target.name.value)
+	new Promise((res,rej)=>{
+		if (this.event.target.age.value>18) {
+			setTimeout(res,4000,`Welcome, ${this.event.target.name.value}. You can vote.`)
+		}else{
+			setTimeout(rej,4000,`Oh sorry ${this.event.target.name.value}. You aren't old enough.`)
+		}
+	}).then((data)=>{
+		alert(data)
+	}).catch((data)=>{
+		alert(data);
+	})
+}
